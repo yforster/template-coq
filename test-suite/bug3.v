96334c31fc11d78297a6faf835a0d83fe1b1d8fe
Yannick Forster <forster@ps.uni-saarland.de>
2018-05-30 16:54:09 +0200

    Updated test cases that failed in sections

--- test-suite/bug3.v
+++ test-suite/bug3.v
@@ -1,9 +1,12 @@
 (** Reported by Randy Pollack **)
+
+(** Note that this was supposed to fail in the past *)
 Require Import Template.Template.
 
 Section foo.
   Variable x : nat.
 
-  Fail Quote Definition this_should_fail := x.
-  Fail Quote Recursively Definition this_should_fail := x.
+  Test Quote x.
+  Quote Recursively Definition this_should_not_fail_anymore := x.
+  Print this_should_not_fail_anymore.
 End foo.
--- test-suite/bug4.v
+++ test-suite/bug4.v
@@ -5,10 +5,3 @@ Definition I (t:Type) (x:t) : t := x.
 Definition II := I (forall t:Type, t -> t) I.
 Quote Recursively Definition qII := II.
 Print qII.
-
-Section foo.
-  Variable x : nat.
-
-  Fail Quote Definition this_should_fail := x.
-  Fail Quote Recursively Definition this_should_fail := x.
-End foo.
